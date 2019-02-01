91ef9757493ee3b93f7f785c749f95574007377c
zipp <zipp@zipps-macbook-pro.local>
2019-02-01 08:35:39 -0600

    Initial Commit

---
+++ itunes store search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  itunes store search
+//
+//  Created by zipp on 2/1/19.
+//  Copyright © 2019 zipp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ itunes store searchTests/itunes_store_searchTests.swift
@@ -0,0 +1,34 @@
+//
+//  itunes_store_searchTests.swift
+//  itunes store searchTests
+//
+//  Created by zipp on 2/1/19.
+//  Copyright © 2019 zipp. All rights reserved.
+//
+
+import XCTest
+@testable import itunes_store_search
+
+class itunes_store_searchTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
